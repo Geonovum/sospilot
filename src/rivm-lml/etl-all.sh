f001b9e5b9dc990fddf9b62015218fc5ed35f360
Just van den Broecke <just@justobjects.nl>
2014-07-07 14:38:35 +0200

    issue #12 use sos-file stream i.s.o. xml from RIVM LML pgcreads fix

--- src/rivm-lml/etl-all.sh
+++ src/rivm-lml/etl-all.sh
@@ -1,20 +1,15 @@
-#!/bin/sh
-#
-# All ETL for Harvesting and Measurements
-#
-
-# Usually required in order to have Python find your package
+# Usually required in order to have Python find your package                                                                                                          
 export PYTHONPATH=.:$PYTHONPATH
 
-db_options="database=sensors host=localhost user=postgres password=postgres schema=rivm_lml"
+# set postgres credentials and options in $pg_options
+. pgcreds.sh
 
 logdir=/var/log/sospilot
 
-stetl -c harvester.cfg -a "$db_options"            >> $logdir/harvester.log 2>&1
+stetl -c harvester.cfg -a "$pg_options"            >> $logdir/harvester.log 2>&1
 
-stetl -c files2measurements.cfg -a "$db_options"   >> $logdir/files2measurements.log 2>&1
+stetl -c files2measurements.cfg -a "$pg_options"   >> $logdir/files2measurements.log 2>&1
 
 http_options="http_host=sensors.geonovum.nl http_port=80 http_user=postgres http_password=postgres  http_path=/sos/service"
 
-$stetl_cmd -c measurements2sos.cfg -a "$options $http_options"  >> $logdir/measurements2sos.log 2>&
-
+stetl -c measurements2sos.cfg -a "$pg_options $http_options"  >> $logdir/measurements2sos.log 2>&1
