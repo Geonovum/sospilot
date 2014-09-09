9bef6b87c9f2b21d3781f143724043e6ff04ed81
Just van den Broecke <just@justobjects.nl>
2014-09-09 17:23:15 +0200

    issue #7 - get stations from WFS in right GeoJSON X,Y axis ordering by adding srsName=EPSG:4326 to WFS req!

--- src/aq-report/input/get-stations.sh
+++ src/aq-report/input/get-stations.sh
@@ -4,4 +4,4 @@
 #
 rm stations.json
 
-wget -O stations.json 'http://sensors.geonovum.nl/gs/ows?service=WFS&request=GetFeature&typeName=sensors:active_stations&outputformat=JSON'
+wget -O stations.json 'http://sensors.geonovum.nl/gs/ows?service=WFS&request=GetFeature&typeName=sensors:active_stations&outputformat=JSON&srsName=EPSG:4326'
