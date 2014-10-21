7b5df304c380086d37d656a7be1e558fb7de86ba
Just van den Broecke <just@justobjects.nl>
2014-10-21 18:49:56 +0200

    issue #19 creds

--- src/weather/pgcreds.sh
+++ src/weather/pgcreds.sh
@@ -1,4 +1,4 @@
 export PGUSER=postgres
 export PGPASSWORD=postgres
 
-export pg_options="host=localhost user=$PGUSER password=$PGPASSWORD database=sensors schema=weather"
+export pg_options="host=localhost port=5432 weewx_db=../../data/weewx/weewx.sdb user=$PGUSER password=$PGPASSWORD database=sensors schema=weather"
