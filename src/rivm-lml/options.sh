4c6bee8b8fe8d3a83b62a20ed4525439100f334a
Just van den Broecke <just@justobjects.nl>
2014-10-27 03:30:05 +0100

    rivm lml host-specific options

--- src/rivm-lml/options.sh
+++ src/rivm-lml/options.sh
@@ -1,4 +1,4 @@
 . ../options-`hostname`.sh
 
-export pg_options="host=$PGHOST user=$PGUSER password=$PGPASSWORD database=sensors schema=rivm_lml"
+export pg_options="host=$PGHOST user=$PGUSER password=$PGPASSWORD port=5432 database=sensors schema=rivm_lml"
 export sos_options="http_host=sensors.geonovum.nl http_port=80 http_user=$SOSUSER http_password=$SOSPASSWORD http_path=/sos/service
