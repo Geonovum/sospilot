22e527eee202afe5670b8ef36394efaccc2e97d2
Just van den Broecke <just@justobjects.nl>
2014-11-11 13:26:22 +0100

    issue #19 - first real kiosk version

--- www/synsite.sh
+++ www/synsite.sh
@@ -1,1 +1,2 @@
-scp index.html  sadmin@sensors:/var/www/sensors.geonovum.nl/site
+scp index.html sadmin@sensors:/var/www/sensors.geonovum.nl/site
+scp kiosk/index.html sadmin@sensors:/var/www/sensors.geonovum.nl/site/kiosk
