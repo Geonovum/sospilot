6bf96f28ce7c7a2b8603f3123543469ef9b56aa1
Just van den Broecke <just@justobjects.nl>
2015-11-10 16:41:51 +0100

    #24 FIWARE extended Orion version with Rush

---
+++ src/fiware/docker/orionrush/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+REDIS_HOME=/opt/redis-${REDIS_VERSION}
+RUSH_HOME=/opt/Rush-${RUSH_VERSION}
+
+cd ${REDIS_HOME}
+redis-server redis.conf > /var/log/redis.log 2>&1 &
+
+cd ${RUSH_HOME}
+export RUSH_GEN_MONGO=${MONGODB_HOST}
+bin/listener > /var/log/rush-listener.log 2>&1 &
+bin/consumer > /var/log/rush-consumer.log 2>&1 &
+
+/usr/bin/contextBroker -fg -multiservice -rush localhost:5001 -logDir /var/log/contextBroker
+
+
