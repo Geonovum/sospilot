4410284a5a3cede881866be82b9008438c3368d1
Just van den Broecke <just@justobjects.nl>
2015-11-12 14:35:23 +0100

    issue #24 : init.d script for FIWARE IoT stack

--- src/fiware/docker/restart.sh
+++ src/fiware/docker/restart.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-YAML=iotarush.yaml
+YAML=/opt/geonovum/sospilot/git/src/fiware/docker/iotarush.yaml
 
 docker-compose -f ${YAML} stop
-docker rm -v $(docker ps -a -q -f status=exited)
+docker rm -v docker_iotacpp_1
+docker rm -v docker_orion_1
+# docker rm -v $(docker ps -a -q -f status=exited)
 docker-compose -f ${YAML} up -d
 docker ps
-
