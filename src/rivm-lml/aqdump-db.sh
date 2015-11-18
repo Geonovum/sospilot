291c93b3687c8b8856618f1b50cdf85e44298a92
Just van den Broecke <just@justobjects.nl>
2015-11-18 17:38:34 +0100

    also include small sample files in LML Shapefile/CSV dumps

--- src/rivm-lml/aqdump-db.sh
+++ src/rivm-lml/aqdump-db.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Dumps all aq data in Postgres from LML and other projects.
 #
-
-
 . options.sh
 
 outdir=/var/www/sensors.geonovum.nl/site/aqdump
@@ -14,9 +12,11 @@ mkdir -p ${outdir}/smartemission/shape
 mkdir -p ${outdir}/smartemission/csv
 
 components="nh3 no no2 so2 o3 pm10 pm25 co"
+time_slice_sql=" where sample_time >=  '2015-08-15' AND sample_time < '2015-08-18'"
 # components="nh3 co"
 for comp in $components
 do
+  # Full dump files
   outfile=${outdir}/lml/shape/lml-${comp}.shp
   sql="SELECT * FROM rivm_lml.v_measurements_$comp"
   pg="host=${PGHOST} user=${PGUSER} dbname=sensors password=${PGPASSWORD}"
@@ -26,6 +26,18 @@ do
   echo "csv gen"
   outfile=${outdir}/lml/csv/lml-${comp}.csv
   ogr2ogr -f CSV ${outfile}  PG:"${pg}" -sql "$sql" -lco GEOMETRY=AS_XY -fieldTypeToString DateTime
+
+  # Small sample files  (couple of days data)
+  outfile=${outdir}/lml/shape/lml-${comp}-small.shp
+  sql="SELECT * FROM rivm_lml.v_measurements_$comp ${time_slice_sql}"
+  pg="host=${PGHOST} user=${PGUSER} dbname=sensors password=${PGPASSWORD}"
+  echo "shapefile small gen"
+  ogr2ogr -f "ESRI Shapefile" ${outfile}  PG:"${pg}" -sql "$sql" -fieldTypeToString DateTime
+
+  echo "csv small gen"
+  outfile=${outdir}/lml/csv/lml-${comp}-small.csv
+  ogr2ogr -f CSV ${outfile}  PG:"${pg}" -sql "$sql" -lco GEOMETRY=AS_XY -fieldTypeToString DateTime
+
 done
 
 components="no2 co o3"
