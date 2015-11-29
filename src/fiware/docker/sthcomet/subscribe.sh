00e59e8e37fba948083c06724d74bcfd3346fb21
Just van den Broecke <just@justobjects.nl>
2015-11-29 17:20:30 +0100

    fix severe bug where Geometry was never added when writing to DB

---
+++ src/fiware/docker/sthcomet/subscribe.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ORION_HOST=sensors.geonovum.nl
+ORION_PORT=1026
+STH_HOST=sensors.geonovum.nl
+STH_PORT=8666
+
+
+curl ${ORION_HOST}:${ORION_PORT}/v1/subscribeContext -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'Fiware-Service: fiwareiot' --header 'Fiware-ServicePath: /' -d @- <<EOF
+{
+    "entities": [
+        {
+            "type": "thing",
+            "isPattern": "false",
+            "id": "NexusEnt1"
+        }
+    ],
+    "attributes": [
+        "temperature"
+    ],
+    "reference": "sthcomet:8666/notify",
+    "duration": "P1M",
+    "notifyConditions": [
+        {
+            "type": "ONCHANGE",
+            "condValues": [
+                "temperature"
+            ]
+        }
+    ],
+    "throttling": "PT1S"
+}
+EOF
