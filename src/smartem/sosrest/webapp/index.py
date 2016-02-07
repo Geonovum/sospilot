bb266b04f452d080c9e91b967455a14e3fd9ab38
Just van den Broecke <just@justobjects.nl>
2016-02-07 16:32:41 +0100

    finalize v1 of SOS REST API SmartEmission - html render

--- src/smartem/sosrest/webapp/index.py
+++ src/smartem/sosrest/webapp/index.py
@@ -29,6 +29,24 @@ def nocache(view):
 
     return update_wrapper(no_cache, view)
 
+# Shorthand to get stations array from DB
+def get_stations():
+    # Do query from DB
+    db = PostGIS(config)
+    return db.do_query('SELECT * from stations', 'stations')
+
+# Shorthand to get (last values) array from DB
+def get_last_values(station):
+    # Do query from DB
+    db = PostGIS(config)
+
+    # Default is to get all last measurements
+    query = 'SELECT * from v_last_measurements'
+    if station:
+        # Last measurements for single station
+        query = query + ' WHERE device_id = ' + station
+    return db.do_query(query, 'v_last_measurements')
+
 # Shorthand to create proper JSON HTTP response
 def make_json_response(json_doc):
     response = make_response(json_doc)
@@ -39,35 +57,43 @@ def make_json_response(json_doc):
 @app.route('/')
 @nocache
 def home():
-    return render_template('home.html')
+    # Fetch stations from DB
+    stations_list = get_stations()
+    return render_template('home.html', stations=stations_list)
 
+# Get last values for single station (as html)
+# Example: /last_updates?station=23&expanded=true
+@app.route('/last_updates')
+@nocache
+def last_updates(station=None):
+    # Get last values, all or for single station if 'station=' arg in query string
+    last_values = get_last_values(request.args.get('station', None))
 
-# Get list of all stations with locations
+    # Construct the response: JSON doc via Jinja2 template with JSON content type
+    return render_template('last_updates.html', last_values=last_values)
+
+# Get list of all stations with locations (as JSON)
 @app.route('/api/v1/stations')
 @nocache
 def stations():
-    # Do query from DB
-    db = PostGIS(config)
-    stations_list = db.do_query('SELECT * from stations', 'stations')
+    # Fetch stations from DB
+    stations_list = get_stations()
 
     # Construct the response: JSON doc via Jinja2 template with JSON content type
     json_doc = render_template('stations.json', stations=stations_list)
     return make_json_response(json_doc)
 
 
-# Get last values for single station
+# Get last values for single station  (as JSON)
 # Example: /api/v1/timeseries?station=23&expanded=true
 @app.route('/api/v1/timeseries')
 @nocache
 def timeseries(package=None):
-    timeseries_list = []
-    station = request.args.get('station', None)
-    if station:
-        db = PostGIS(config)
-        timeseries_list = db.do_query('SELECT * from v_last_measurements WHERE device_id = %s' % station, 'v_last_measurements')
+    # Get last values, all or for single station if 'station=' arg in query string
+    last_values = get_last_values(request.args.get('station', None))
 
     # Construct the response: JSON doc via Jinja2 template with JSON content type
-    json_doc = render_template('timeseries.json', timeseries=timeseries_list)
+    json_doc = render_template('timeseries.json', last_values=last_values)
     return make_json_response(json_doc)
 
 if __name__ == '__main__':
